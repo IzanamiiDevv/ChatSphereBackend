2922741442e438ac0536b705faee3f482579da34
IzanamiiDevv <rafaeloli596@gmail.com>
2025-04-24 21:52:04 +0800

    Add Backend Code

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+const PORT = 3400;
+const client = "http://localhost:5173";
+const app = express();
+const server = http.createServer(app);
+
+// SocketIO
+const io = new Server(server, {
+    cors: {
+        origin: client,
+        methods: ["GET", "POST"]
+    }
+});
+
+
+const chats = [];
+io.on("connection", (socket) => {
+    console.log("New Connection");
+
+    socket.on("rendered", () => {
+        io.emit("createChat", chats);
+    })
+
+    socket.on("sendChat", (data) => {
+        chats.push(data);
+        io.emit("createChat", chats);
+    });
+});
+
+
+// ExpressJS
+app.use(cors({
+    origin: client
+}));
+
+app.get("/", (req, res) => {
+    res.status(200).send("Server is Active");
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is Listening at Port: ${PORT}`);
+});
